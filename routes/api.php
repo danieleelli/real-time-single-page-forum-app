f01589250e218cfb09a76ab6e9672bfed5cc91f5
danieleelli <d.campanelli@gmail.com>
2018-09-12 16:07:02 +0200

    Like and Dislike System for Reply

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,5 @@ Route::apiResource('/category', 'CategoryController');
 
 Route::apiResource('/question/{question}/reply', 'ReplyController');
 
+Route::post('/like/{reply}', 'LikeController@likeIt');
+Route::delete('/like/{reply}', 'LikeController@unlikeIt');
